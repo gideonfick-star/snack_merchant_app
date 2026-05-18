21dfcf59a358c8bc2acd9e7ae5512c5f12ec49b0
Gideon Fick <gideonfick@gmail.com>
2026-05-19 01:44:17 +0200

    Fix Render dynamic port

--- snack_merchant_backend/server.js
+++ snack_merchant_backend/server.js
@@ -251,7 +251,7 @@ app.post("/product-stock", async (req, res) => {
     });
   }
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
